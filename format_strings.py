7f8b6a342a89ee231279a567c3a787ac4b985e44
Ankushkushwaha0009 <93570548+Ankushkushwaha0009@users.noreply.github.com>
2022-09-17 22:47:34 +0530

    Create format_strings.py

---
+++ format_strings.py
@@ -0,0 +1,20 @@
+# animal = "cow"
+# item = "moon"
+# print("the {} jumped over {} ".format("cow" , "moon"))
+# print("the {0} jumped over {1} ".format("cow" , "moon"))
+# print("the {1} jumped over {1} ".format("cow" , "moon"))
+# print("the {0} jumped over {0} ".format("cow" , "moon"))
+
+# print("the {animal} jumped over {item} ".format(animal = "cow" ,item = "moon"))
+
+# print(f'the {animal} jumped over the {item} ')
+
+a = 102.33333333
+print("the number is {:.2f}".format(a))  # printing upto 2 decimals only 
+
+print("the binary number is {:b}".format(a))
+
+print("the octal  number is {:o}".format(a))
+
+print("the number is hexadecimal {:X}".format(a))
+
