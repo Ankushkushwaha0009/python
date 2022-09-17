0271ccc6768c3cd4424a5c6e7929d441a5675991
Ankushkushwaha0009 <93570548+Ankushkushwaha0009@users.noreply.github.com>
2022-09-17 22:42:51 +0530

    Create storing_function_intothe_variable.py

---
+++ storing_function_intothe_variable.py
@@ -0,0 +1,19 @@
+# def hello():
+#     print("hello")
+
+# print(hello)  # it will show the address of that function
+# hi = hello
+# # it will give the same result 
+
+# hi()
+# hello()
+
+
+# s = print  # we store the address of the print in hi 
+# # # it will give the same result 
+
+# s("hi ankush")
+# print("hi ankush")
+
+
+
