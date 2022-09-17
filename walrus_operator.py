3bfdbed86d3629cfb8d92e5bb1ec3c71e2bfae6e
Ankushkushwaha0009 <93570548+Ankushkushwaha0009@users.noreply.github.com>
2022-09-17 22:40:15 +0530

    Create walrus_operator.py

---
+++ walrus_operator.py
@@ -0,0 +1,36 @@
+# from ast import operator
+
+# walrus operator --> :=
+
+
+# happy = True
+# print(happy)
+
+# invalid 
+# print(happy = True)
+
+# print( happy := True)
+
+# foods  = list()
+
+# while True:
+#     food = input("what food do you like ? : ")
+#     if food == "quit":
+#         break
+#     foods.append(food)
+
+# print(foods)
+
+# using walrus operator 
+
+# foods  = list()
+# while food := input("what food do you like? :") != "quit":
+#     foods.append(food)
+
+
+b = 10
+if a := int(input("Enter : "))  > b :
+    print("a is greater")
+else:
+    print("b is graeter ")       
+
